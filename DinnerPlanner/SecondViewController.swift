26ca5af9dfa7dfaca58d41cf77bb22335a20ae60
Marco Bibrich <marco.bibrich@students.fhnw.ch>
2017-06-18 16:43:32 +0200

    added Search interface and logic for datepicker

--- DinnerPlanner/SecondViewController.swift
+++ DinnerPlanner/SecondViewController.swift
@@ -10,9 +10,27 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
+    
+    @IBOutlet weak var datePickerText: UITextField!
+    @IBOutlet weak var locationText: UITextField!
+    @IBOutlet weak var groupNameText: UITextField!
+    
+    
+    let datePicker = UIDatePicker()
+    
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        makeDatePicker()
+        
+        
+        
+        //locationText.inputAccessoryView = createToolbarWithDoneButton()
+        //groupNameText.inputAccessoryView = createToolbarWithDoneButton()
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +38,37 @@ class SecondViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    func makeDatePicker(){
+    //datepicker after tutorial: youtube.com/watch?v=_ADJxJ7pjRk#t=426.928552063
+        datePicker.datePickerMode = .dateAndTime
+        
+        let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target:nil,action: #selector (donePressed))
+        // toolbar
+        
+        
+        let toolbar = UIToolbar()
+        toolbar.sizeToFit()
+        toolbar.setItems([doneButton], animated: false)
 
+        
+        //connect datepicker with label
+        datePickerText.inputAccessoryView = toolbar
+        datePickerText.inputView = datePicker
+        
+    }
+    
+    func donePressed(){
+        //format date
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .full
+        dateFormatter.timeStyle = .short
+        
+        datePickerText.text = dateFormatter.string(from:datePicker.date)
+        
+        
+        self.view.endEditing(true)
+    }
 }
+
 
